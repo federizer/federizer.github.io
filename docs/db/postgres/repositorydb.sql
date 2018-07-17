185e347ad906bb7d8e37eaa832586c5687edf41e
1token <1token@federizer.org>
2018-07-17 14:26:24 +0200

    schema update

--- docs/db/postgres/repositorydb.sql
+++ docs/db/postgres/repositorydb.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 10.1
 -- Dumped by pg_dump version 10.4
 
--- Started on 2018-07-11 12:45:19
+-- Started on 2018-07-17 14:23:03
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -18,7 +18,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 1 (class 3079 OID 12924)
+-- TOC entry 2 (class 3079 OID 12924)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -26,8 +26,8 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2876 (class 0 OID 0)
--- Dependencies: 1
+-- TOC entry 2877 (class 0 OID 0)
+-- Dependencies: 2
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -35,7 +35,24 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
 --
--- TOC entry 217 (class 1255 OID 21201)
+-- TOC entry 1 (class 3079 OID 21774)
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA pg_catalog;
+
+
+--
+-- TOC entry 2878 (class 0 OID 0)
+-- Dependencies: 1
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
+--
+-- TOC entry 210 (class 1255 OID 21785)
 -- Name: delete_message_view_func(); Type: FUNCTION; Schema: public; Owner: admin
 --
 
@@ -43,12 +60,12 @@ CREATE FUNCTION public.delete_message_view_func() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
 DECLARE
-principal_id uuid;
+account_id uuid;
 BEGIN
-  principal_id := (SELECT id FROM "public".principal WHERE uupn = OLD.sender);
-  IF principal_id IS NULL
+  account_id := (SELECT id FROM "public".account WHERE uupn = OLD.sender);
+  IF account_id IS NULL
   THEN
-    RAISE EXCEPTION 'Principal % is not registered.', OLD.sender;
+    RAISE EXCEPTION 'Account % doesn\'t exist.', OLD.sender;
   END IF;
 
   DELETE FROM message WHERE id = OLD.id;
@@ -60,7 +77,7 @@ $$;
 ALTER FUNCTION public.delete_message_view_func() OWNER TO admin;
 
 --
--- TOC entry 210 (class 1255 OID 21042)
+-- TOC entry 218 (class 1255 OID 21786)
 -- Name: insert_message_view_func(); Type: FUNCTION; Schema: public; Owner: admin
 --
 
@@ -68,16 +85,19 @@ CREATE FUNCTION public.insert_message_view_func() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
 DECLARE
-principal_id uuid;
+account_id uuid;
 BEGIN
-  principal_id := (SELECT id FROM "public".principal WHERE uupn = NEW.sender);
-  IF principal_id IS NULL
+  account_id := (SELECT id FROM "public".account WHERE uupn = NEW.sender);
+  IF account_id IS NULL
   THEN
-    RAISE EXCEPTION 'Principal % is not registered.', NEW.sender;
+    -- RAISE EXCEPTION 'Account % doesn\'t exist.', NEW.sender;
+	account_id = uuid_generate_v4();
+	INSERT INTO account(id, uupn)
+    VALUES (account_id, NEW.sender);
   END IF;
   
   INSERT INTO message(id, mime_type, received_at, sent_at, subject, created_at, uufid, uumid, uupid, uurn, meta, body, plaintext, sender_id)
-  VALUES (NEW.id, NEW.mime_type, NEW.received_at, NEW.sent_at, NEW.subject, NEW.created_at, NEW.uufid, NEW.uumid, NEW.uupid, NEW.uurn, NEW.meta, NEW.body, NEW.plaintext, principal_id);
+  VALUES (NEW.id, NEW.mime_type, NEW.received_at, NEW.sent_at, NEW.subject, NEW.created_at, NEW.uufid, NEW.uumid, NEW.uupid, NEW.uurn, NEW.meta, NEW.body, NEW.plaintext, account_id);
   RETURN NEW;
 END;
 $$;
@@ -86,7 +106,7 @@ $$;
 ALTER FUNCTION public.insert_message_view_func() OWNER TO admin;
 
 --
--- TOC entry 211 (class 1255 OID 21228)
+-- TOC entry 219 (class 1255 OID 21787)
 -- Name: update_message_view_func(); Type: FUNCTION; Schema: public; Owner: admin
 --
 
@@ -94,12 +114,12 @@ CREATE FUNCTION public.update_message_view_func() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
 DECLARE
-principal_id uuid;
+account_id uuid;
 BEGIN
-  principal_id := (SELECT id FROM "public".principal WHERE uupn = NEW.sender);
-  IF principal_id IS NULL
+  account_id := (SELECT id FROM "public".account WHERE uupn = NEW.sender);
+  IF account_id IS NULL
   THEN
-    RAISE EXCEPTION 'Principal % is not registered.', NEW.sender;
+    RAISE EXCEPTION 'Account % doesn\'t exist.', NEW.sender;
   END IF;
   
   UPDATE message SET
@@ -118,7 +138,21 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 196 (class 1259 OID 20929)
+-- TOC entry 204 (class 1259 OID 21824)
+-- Name: account; Type: TABLE; Schema: public; Owner: admin
+--
+
+CREATE TABLE public.account (
+    id uuid NOT NULL,
+    uupn character varying(255) NOT NULL,
+    identity_id uuid
+);
+
+
+ALTER TABLE public.account OWNER TO admin;
+
+--
+-- TOC entry 197 (class 1259 OID 21788)
 -- Name: attachment; Type: TABLE; Schema: public; Owner: admin
 --
 
@@ -130,14 +164,14 @@ CREATE TABLE public.attachment (
     uufid uuid NOT NULL,
     message_id uuid NOT NULL,
     meta jsonb,
-	plaintext text
+    plaintext text
 );
 
 
 ALTER TABLE public.attachment OWNER TO admin;
 
 --
--- TOC entry 197 (class 1259 OID 20939)
+-- TOC entry 198 (class 1259 OID 21794)
 -- Name: envelope; Type: TABLE; Schema: public; Owner: admin
 --
 
@@ -153,7 +187,7 @@ CREATE TABLE public.envelope (
 ALTER TABLE public.envelope OWNER TO admin;
 
 --
--- TOC entry 198 (class 1259 OID 20947)
+-- TOC entry 199 (class 1259 OID 21800)
 -- Name: envelope_label; Type: TABLE; Schema: public; Owner: admin
 --
 
@@ -167,7 +201,7 @@ CREATE TABLE public.envelope_label (
 ALTER TABLE public.envelope_label OWNER TO admin;
 
 --
--- TOC entry 199 (class 1259 OID 20952)
+-- TOC entry 200 (class 1259 OID 21803)
 -- Name: envelope_properties; Type: TABLE; Schema: public; Owner: admin
 --
 
@@ -184,7 +218,7 @@ CREATE TABLE public.envelope_properties (
 ALTER TABLE public.envelope_properties OWNER TO admin;
 
 --
--- TOC entry 203 (class 1259 OID 20989)
+-- TOC entry 201 (class 1259 OID 21812)
 -- Name: identity; Type: TABLE; Schema: public; Owner: admin
 --
 
@@ -196,7 +230,7 @@ CREATE TABLE public.identity (
 ALTER TABLE public.identity OWNER TO admin;
 
 --
--- TOC entry 201 (class 1259 OID 20972)
+-- TOC entry 202 (class 1259 OID 21815)
 -- Name: message; Type: TABLE; Schema: public; Owner: admin
 --
 
@@ -215,15 +249,15 @@ CREATE TABLE public.message (
     meta jsonb,
     received timestamp without time zone,
     sent timestamp without time zone,
-	body text,
-	plaintext text 
+    body text,
+    plaintext text
 );
 
 
 ALTER TABLE public.message OWNER TO admin;
 
 --
--- TOC entry 202 (class 1259 OID 20982)
+-- TOC entry 203 (class 1259 OID 21821)
 -- Name: message_properties; Type: TABLE; Schema: public; Owner: admin
 --
 
@@ -238,21 +272,7 @@ CREATE TABLE public.message_properties (
 ALTER TABLE public.message_properties OWNER TO admin;
 
 --
--- TOC entry 200 (class 1259 OID 20965)
--- Name: principal; Type: TABLE; Schema: public; Owner: admin
---
-
-CREATE TABLE public.principal (
-    id uuid NOT NULL,
-    uupn character varying(255) NOT NULL,
-    identity_id uuid
-);
-
-
-ALTER TABLE public.principal OWNER TO admin;
-
---
--- TOC entry 204 (class 1259 OID 21256)
+-- TOC entry 205 (class 1259 OID 21827)
 -- Name: message_view; Type: VIEW; Schema: public; Owner: admin
 --
 
@@ -268,15 +288,15 @@ CREATE VIEW public.message_view AS
     message.uupid,
     message.uurn,
     message.meta,
-    principal.uupn AS sender
+    account.uupn AS sender
    FROM (public.message
-     JOIN public.principal ON ((message.sender_id = principal.id)));
+     JOIN public.account ON ((message.sender_id = account.id)));
 
 
 ALTER TABLE public.message_view OWNER TO admin;
 
 --
--- TOC entry 2711 (class 2606 OID 20936)
+-- TOC entry 2712 (class 2606 OID 21832)
 -- Name: attachment attachment_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -285,7 +305,7 @@ ALTER TABLE ONLY public.attachment
 
 
 --
--- TOC entry 2717 (class 2606 OID 20951)
+-- TOC entry 2718 (class 2606 OID 21834)
 -- Name: envelope_label envelope_label_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -294,7 +314,7 @@ ALTER TABLE ONLY public.envelope_label
 
 
 --
--- TOC entry 2715 (class 2606 OID 20946)
+-- TOC entry 2716 (class 2606 OID 21836)
 -- Name: envelope envelope_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -303,7 +323,7 @@ ALTER TABLE ONLY public.envelope
 
 
 --
--- TOC entry 2719 (class 2606 OID 20962)
+-- TOC entry 2720 (class 2606 OID 21838)
 -- Name: envelope_properties envelope_properties_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -312,16 +332,16 @@ ALTER TABLE ONLY public.envelope_properties
 
 
 --
--- TOC entry 2723 (class 2606 OID 20969)
--- Name: principal identity_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
+-- TOC entry 2734 (class 2606 OID 21840)
+-- Name: account identity_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
-ALTER TABLE ONLY public.principal
+ALTER TABLE ONLY public.account
     ADD CONSTRAINT identity_pkey PRIMARY KEY (id);
 
 
 --
--- TOC entry 2735 (class 2606 OID 20993)
+-- TOC entry 2724 (class 2606 OID 21842)
 -- Name: identity identity_pkey1; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -330,7 +350,7 @@ ALTER TABLE ONLY public.identity
 
 
 --
--- TOC entry 2727 (class 2606 OID 20979)
+-- TOC entry 2726 (class 2606 OID 21844)
 -- Name: message message_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -339,7 +359,7 @@ ALTER TABLE ONLY public.message
 
 
 --
--- TOC entry 2731 (class 2606 OID 20986)
+-- TOC entry 2730 (class 2606 OID 21846)
 -- Name: message_properties message_properties_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -348,7 +368,7 @@ ALTER TABLE ONLY public.message_properties
 
 
 --
--- TOC entry 2713 (class 2606 OID 20938)
+-- TOC entry 2714 (class 2606 OID 21848)
 -- Name: attachment uk_attachment_uufid; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -357,7 +377,7 @@ ALTER TABLE ONLY public.attachment
 
 
 --
--- TOC entry 2721 (class 2606 OID 20964)
+-- TOC entry 2722 (class 2606 OID 21850)
 -- Name: envelope_properties uk_i2quyacqckuwwy7meak8xeqap; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -366,16 +386,16 @@ ALTER TABLE ONLY public.envelope_properties
 
 
 --
--- TOC entry 2725 (class 2606 OID 20971)
--- Name: principal uk_identity_upn; Type: CONSTRAINT; Schema: public; Owner: admin
+-- TOC entry 2736 (class 2606 OID 21852)
+-- Name: account uk_identity_upn; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
-ALTER TABLE ONLY public.principal
+ALTER TABLE ONLY public.account
     ADD CONSTRAINT uk_identity_upn UNIQUE (uupn);
 
 
 --
--- TOC entry 2729 (class 2606 OID 20981)
+-- TOC entry 2728 (class 2606 OID 21854)
 -- Name: message uk_message_uufid; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -384,7 +404,7 @@ ALTER TABLE ONLY public.message
 
 
 --
--- TOC entry 2733 (class 2606 OID 20988)
+-- TOC entry 2732 (class 2606 OID 21856)
 -- Name: message_properties uk_o7txfb0shd3p577wyd7ikrfhv; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -393,7 +413,7 @@ ALTER TABLE ONLY public.message_properties
 
 
 --
--- TOC entry 2745 (class 2620 OID 21261)
+-- TOC entry 2745 (class 2620 OID 21857)
 -- Name: message_view delete_message_view_trig; Type: TRIGGER; Schema: public; Owner: admin
 --
 
@@ -401,7 +421,7 @@ CREATE TRIGGER delete_message_view_trig INSTEAD OF DELETE ON public.message_view
 
 
 --
--- TOC entry 2744 (class 2620 OID 21260)
+-- TOC entry 2746 (class 2620 OID 21858)
 -- Name: message_view insert_message_view_trig; Type: TRIGGER; Schema: public; Owner: admin
 --
 
@@ -409,7 +429,7 @@ CREATE TRIGGER insert_message_view_trig INSTEAD OF INSERT ON public.message_view
 
 
 --
--- TOC entry 2746 (class 2620 OID 21262)
+-- TOC entry 2747 (class 2620 OID 21859)
 -- Name: message_view update_message_view_trig; Type: TRIGGER; Schema: public; Owner: admin
 --
 
@@ -417,7 +437,16 @@ CREATE TRIGGER update_message_view_trig INSTEAD OF UPDATE ON public.message_view
 
 
 --
--- TOC entry 2736 (class 2606 OID 20994)
+-- TOC entry 2744 (class 2606 OID 21895)
+-- Name: account fk_account_identity_id; Type: FK CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.account
+    ADD CONSTRAINT fk_account_identity_id FOREIGN KEY (identity_id) REFERENCES public.identity(id);
+
+
+--
+-- TOC entry 2737 (class 2606 OID 21860)
 -- Name: attachment fk_attachment_message_id; Type: FK CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -426,7 +455,7 @@ ALTER TABLE ONLY public.attachment
 
 
 --
--- TOC entry 2739 (class 2606 OID 21009)
+-- TOC entry 2740 (class 2606 OID 21865)
 -- Name: envelope_label fk_envelope_label_envelope_id; Type: FK CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -435,7 +464,7 @@ ALTER TABLE ONLY public.envelope_label
 
 
 --
--- TOC entry 2738 (class 2606 OID 21004)
+-- TOC entry 2738 (class 2606 OID 21870)
 -- Name: envelope fk_envelope_message_id; Type: FK CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -444,7 +473,7 @@ ALTER TABLE ONLY public.envelope
 
 
 --
--- TOC entry 2740 (class 2606 OID 21014)
+-- TOC entry 2741 (class 2606 OID 21875)
 -- Name: envelope_properties fk_envelope_properties_envelope_id; Type: FK CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -453,16 +482,16 @@ ALTER TABLE ONLY public.envelope_properties
 
 
 --
--- TOC entry 2737 (class 2606 OID 20999)
--- Name: envelope fk_envelope_recipient_principal_id; Type: FK CONSTRAINT; Schema: public; Owner: admin
+-- TOC entry 2739 (class 2606 OID 21880)
+-- Name: envelope fk_envelope_recipient_account_id; Type: FK CONSTRAINT; Schema: public; Owner: admin
 --
 
 ALTER TABLE ONLY public.envelope
-    ADD CONSTRAINT fk_envelope_recipient_principal_id FOREIGN KEY (recipient_id) REFERENCES public.principal(id);
+    ADD CONSTRAINT fk_envelope_recipient_account_id FOREIGN KEY (recipient_id) REFERENCES public.account(id);
 
 
 --
--- TOC entry 2743 (class 2606 OID 21029)
+-- TOC entry 2743 (class 2606 OID 21885)
 -- Name: message_properties fk_message_properties_message_id; Type: FK CONSTRAINT; Schema: public; Owner: admin
 --
 
@@ -471,24 +500,15 @@ ALTER TABLE ONLY public.message_properties
 
 
 --
--- TOC entry 2742 (class 2606 OID 21024)
--- Name: message fk_message_sender_principal_id; Type: FK CONSTRAINT; Schema: public; Owner: admin
+-- TOC entry 2742 (class 2606 OID 21890)
+-- Name: message fk_message_sender_account_id; Type: FK CONSTRAINT; Schema: public; Owner: admin
 --
 
 ALTER TABLE ONLY public.message
-    ADD CONSTRAINT fk_message_sender_principal_id FOREIGN KEY (sender_id) REFERENCES public.principal(id);
+    ADD CONSTRAINT fk_message_sender_account_id FOREIGN KEY (sender_id) REFERENCES public.account(id);
 
 
---
--- TOC entry 2741 (class 2606 OID 21019)
--- Name: principal fk_principal_identity_id; Type: FK CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY public.principal
-    ADD CONSTRAINT fk_principal_identity_id FOREIGN KEY (identity_id) REFERENCES public.identity(id);
-
-
--- Completed on 2018-07-11 12:45:19
+-- Completed on 2018-07-17 14:23:03
 
 --
 -- PostgreSQL database dump complete
