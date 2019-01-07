796c95ffc14e047d5850edf9ad96a38a2d9d1d60
1token <1token@federizer.com>
2019-01-07 20:57:14 +0100

    sql

--- docs/db/postgres/mail-data.sql
+++ docs/db/postgres/mail-data.sql
@@ -1,10 +1,20 @@
+--mail.message-------------------------------------------------------------------------------------------------------------
+INSERT INTO mail.message
+(sender_email_address, sender_display_name, subject, body, sent_at)
+VALUES('jdoe@leadict.com', 'John Doe', 'Hello', 'Hello World!', '2019-01-06 10:21:04');
+INSERT INTO mail.message
+(sender_email_address, sender_display_name, subject, body, sent_at)
+VALUES('jdoe@leadict.com', 'John Doe', 'Allo', 'Allo, Allo!', '2019-01-07 12:41:53');
+INSERT INTO mail.message
+(sender_email_address, sender_display_name, subject, body, sent_at)
+VALUES('izboran@gmail.com', 'Igor Zboran', 'Hello again', 'Hello everybody!', '2019-01-07 17:08:29');
 INSERT INTO mail.message
 (sender_email_address, sender_display_name, subject, body)
-VALUES('jdoe@leadict.com', 'John Doe', 'Hello', 'Hello World!');
+VALUES('izboran@gmail.com', 'Igor Zboran', 'Invoice1', '124€');
 INSERT INTO mail.message
 (sender_email_address, sender_display_name, subject, body)
-VALUES('jdoe@leadict.com', 'John Doe', 'Hello again', 'Hello everybody!');
-
+VALUES('izboran@gmail.com', 'Igor Zboran', 'Invoice2', '15,25€');
+--mail.envelope-------------------------------------------------------------------------------------------------------------
 INSERT INTO mail.envelope
 (message_id, "type", recipient_email_address, recipient_display_name)
 VALUES(1, 'to', 'izboran@gmail.com', 'Igor Zboran');
@@ -24,3 +34,27 @@ VALUES(2, 'cc', 'hfinn@leadict.com', 'Huckleberry Finn');
 INSERT INTO mail.envelope
 (message_id, "type", recipient_email_address, recipient_display_name)
 VALUES(2, 'bcc', 'tsawyer@leadict.com', 'Tom Sawyer');
+
+INSERT INTO mail.envelope
+(message_id, "type", recipient_email_address, recipient_display_name)
+VALUES(3, 'to', 'jdoe@leadict.com', 'John Doe');
+INSERT INTO mail.envelope
+(message_id, "type", recipient_email_address, recipient_display_name)
+VALUES(3, 'cc', 'tsawyer@leadict.com', 'Tom Sawyer');
+
+INSERT INTO mail.envelope
+(message_id, "type", recipient_email_address, recipient_display_name)
+VALUES(4, 'to', 'jdoe@leadict.com', 'John Doe');
+--labels.system_label-------------------------------------------------------------------------------------------------------------
+INSERT INTO labels.system_label
+(owner, message_id, folder, unread)
+VALUES('izboran@gmail.com', 3, 'sent', true);
+
+INSERT INTO labels.system_label
+(owner, message_id, folder, unread)
+VALUES('jdoe@leadict.com', 3, 'inbox', true);
+
+INSERT INTO labels.system_label
+(owner, message_id, folder, unread)
+VALUES('tsawyer@leadict.com', 3, 'inbox', false);
+
--- docs/db/postgres/mail-fn.sql
+++ docs/db/postgres/mail-fn.sql
@@ -4,6 +4,9 @@ CREATE OR REPLACE FUNCTION mail.read_message(IN _owner character varying, IN _me
                 subject text,
                 body text,
                 recipient jsonb,
+                folder labels.folders,
+                label jsonb,
+                sent_at timestamptz,
                 created_at timestamptz,
                 updated_at timestamptz) AS
 $BODY$
@@ -17,21 +20,29 @@ begin
 				mail.message.subject,
 				mail.message.body,
 				jsonb_object_agg(type, s.recipient ORDER BY type) AS recipient,
+				labels.system_label.folder AS folder,
+				--jsonb_build_array(CASE WHEN labels.system_label.done THEN 'done' END, CASE WHEN labels.system_label.unread THEN 'unread' END) AS label,
+				to_jsonb(array_remove(ARRAY[CASE WHEN labels.system_label.done THEN 'done' ELSE NULL END, CASE WHEN labels.system_label.unread THEN 'unread' ELSE NULL END], NULL)) AS label,
+				mail.message.sent_at,
 				mail.message.created_at,
 				mail.message.updated_at
 	FROM mail.message LEFT JOIN (SELECT
 			mail.envelope.message_id AS message_id,
 			mail.envelope.type AS type,
-	    json_agg(jsonb_build_object('email_address', mail.envelope.recipient_email_address, 'display_name', mail.envelope.recipient_display_name) ORDER BY mail.envelope.id) 
+	    jsonb_agg(jsonb_build_object('email_address', mail.envelope.recipient_email_address, 'display_name', mail.envelope.recipient_display_name) ORDER BY mail.envelope.id) 
 	         AS recipient
 	FROM
 	    mail.envelope 
 	GROUP BY
 	    mail.envelope.message_id, mail.envelope.type) s
 	ON mail.message.id = s.message_id
-	WHERE _message_id IS NULL OR mail.message.id = _message_id    
+	LEFT JOIN labels.system_label
+  	ON mail.message.id = labels.system_label.message_id
+	WHERE (_message_id IS NULL OR mail.message.id = _message_id) AND
+		  (mail.message.sender_email_address = _owner OR (mail.message.sent_at IS NOT NULL AND s.recipient @> ('[{"email_address":"' || _owner || '"}]')::jsonb)) AND
+		  (_system_label_folder IS NULL OR (labels.system_label.folder = _system_label_folder AND labels.system_label.owner = _owner))
 	GROUP BY
-	    mail.message.id;
+	    mail.message.id, labels.system_label.folder, labels.system_label.done, labels.system_label.unread;
 END;	   
 $BODY$
 LANGUAGE plpgsql VOLATILE;
